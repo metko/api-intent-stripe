2805440db0d1da4e1e5a4379c36f2d1975b5cc90
Firstname Lastname <you@example.com>
2020-07-14 10:30:56 +0400

    first

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+const stripe = require('stripe')('sk_test_51H4SxWLVOH5lsckxVgT49pMO5oVK88TqSBFQGvffoJZYFyYezMLMTwWiKw4l00tAmbotK6DOtTTGUbukpUJi3A9U00esYnIMWk');
+
+app.get('/', async (req, res) => {
+ console.log('home')
+ 
+ return "hello"
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body.intent)
+});
+
+app.post('/intent', async (req, res) => {
+    const paymentIntent = await stripe.paymentIntents.create(req.body.intent);
+    res.json({client_secret: paymentIntent.client_secret});
+
+});
+
+app.listen(80, () => {
+  console.log('Running on localhost');
+});
