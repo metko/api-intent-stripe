4dd004581c2883665a979fb296cc079bc5739dd3
Firstname Lastname <you@example.com>
2020-07-14 10:51:49 +0400

    return intent objct instead of client secreet

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post('/', function (req, res) {
 app.post('/intent', async (req, res) => {
     if(!req.body.intent) return res.send('No intent object passed...')
     const paymentIntent = await stripe.paymentIntents.create(req.body.intent);
-    res.json({client_secret: paymentIntent.client_secret});
+    res.json({intent: paymentIntent});
 
 });
 
